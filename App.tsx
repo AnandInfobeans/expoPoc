8d58683007136017bf289827670a3a52d417d753
AnandInfobeans <anand.mathe@infobeans.com>
2025-05-14 18:45:31 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    if (!__DEV__) {
+      onFetchUpdateAsync();
+    }
+  }, []);
+
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        onUpdate();
+      }
+    } catch (error) {
+      console.log(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+  const onUpdate = async () => {
+    await Updates.fetchUpdateAsync();
+    await Updates.reloadAsync();
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.txt}>{`Hii , This update from EXPO (OTA)`}</Text>
+      {/* <Image source={require("./assets/reactlogo.png")} style={styles.img} /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  img: {
+    height: 100,
+    width: 100,
+    backgroundColor: "grey",
+    borderRadius: 10,
+    marginTop: 15,
+  },
+  txt: {
+    fontSize: 16,
+    fontWeight: "700",
+    color: "dodgerblue",
   },
 });
