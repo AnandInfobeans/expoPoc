90d55f11091ce5b2fa68875003188006eabd8a29
Anand.mathe <anand.mathe@infobeans.com>
2025-08-26 00:03:39 +0530

    Text

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -24,7 +24,7 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "yellow",
     alignItems: "center",
     justifyContent: "center",
   },
